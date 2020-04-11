065440fbaa2f2cbe947d515854a8e1c00dde50c2
khalid7am <38160434+khalid7am@users.noreply.github.com>
2020-04-11 15:58:55 +0100

    Update config.php

--- inc/config.php
+++ inc/config.php
@@ -3,14 +3,4 @@
 require 'dbconn.php';
 session_start();
 
-define('Produits', array( 1 => ['id' => 1 , 'libelle' => "CHEMISE STRUCTURÉE 1", 'prix' => "190", 'quantite' => "1", 'image' => "product-1"],
-                          2 => ['id' => 2 ,'libelle' => "CHEMISE STRUCTURÉE 2", 'prix' => "190", 'quantite' => "1", 'image' => "product-2"],
-                          3 => ['id' => 3 ,'libelle' => "CHEMISE STRUCTURÉE 3", 'prix' => "190", 'quantite' => "1", 'image' => "product-3"],
-                          4 => ['id' => 4 ,'libelle' => "CHEMISE STRUCTURÉE 4", 'prix' => "190", 'quantite' => "1", 'image' => "product-4"],
-                          5 => ['id' => 5 ,'libelle' => "CHEMISE STRUCTURÉE 5", 'prix' => "190", 'quantite' => "1", 'image' => "product-5"],
-                          6 => ['id' => 6 ,'libelle' => "CHEMISE STRUCTURÉE 6", 'prix' => "190", 'quantite' => "1", 'image' => "product-6"],
-                          7 => ['id' => 7 ,'libelle' => "CHEMISE STRUCTURÉE 7", 'prix' => "190", 'quantite' => "1", 'image' => "product-7"],
-                          8 => ['id' => 8 ,'libelle' => "CHEMISE STRUCTURÉE 8", 'prix' => "190", 'quantite' => "1", 'image' => "product-8"],
-));
-
 ?>
