3d600709c6ab76b6eb5532791b3d8a1683b3f007
khalid7am <38160434+khalid7am@users.noreply.github.com>
2020-04-11 15:56:48 +0100

    Update profile.php

--- profile.php
+++ profile.php
@@ -21,55 +21,7 @@
 	</div>
 	<section class="ftco-section contact-section bg-light">
 		<div class="container">
-			<!--div class="row d-flex mb-5 contact-info">
-				<div class="w-100"></div>
-				<div class="col-md-3 d-flex">
-					<div class="info bg-white p-4">
-						<p><span>Address:</span> Rue Almoqawama Imm. B Agadir, Maroc</p>
-					</div>
-				</div>
-				<div class="col-md-3 d-flex">
-					<div class="info bg-white p-4">
-						<p><span>Phone:</span> <a href="tel://1234567920">+212 6 00 00 00 00</a></p>
-					</div>
-				</div>
-				<div class="col-md-3 d-flex">
-					<div class="info bg-white p-4">
-						<p><span>Email:</span> <a href="mailto:info@yoursite.com">info@mysweater.com</a></p>
-					</div>
-				</div>
-				<div class="col-md-3 d-flex">
-					<div class="info bg-white p-4">
-						<p><span>Website</span> <a href="#">mysweater.com</a></p>
-					</div>
-				</div>
-			</div>
-			<div class="row block-9">
-				<div class="col-md-6 order-md-last d-flex">
-					<form action="#" class="bg-white p-5 contact-form">
-						<div class="form-group">
-							<input class="form-control" placeholder="Notre nom" type="text">
-						</div>
-						<div class="form-group">
-							<input class="form-control" placeholder="Votre Email" type="text">
-						</div>
-						<div class="form-group">
-							<input class="form-control" placeholder="Sujet" type="text">
-						</div>
-						<div class="form-group">
-							<textarea class="form-control" cols="30" id="" name="" placeholder="Message" rows="7"></textarea>
-						</div>
-						<div class="form-group">
-							<input class="btn btn-primary py-3 px-5" type="submit" value="Envoyer">
-						</div>
-					</form>
-				</div>
-				<div class="col-md-6 d-flex">
-					<div>
-						<img src="images/logo.png" style="display: block; margin: 0 auto" width="83%" height="80%" alt="">
-					</div>
-				</div>
-			</div-->
+			
 		</div>
 	</section>
   <?php require 'inc/footer.php'; ?>
